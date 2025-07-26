4c064510d33f8cc15d0996200f017f918119b7fa
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-26 14:41:05 -0300

    Salvando entidade associada para um PARTE 2 (com recuparação do nome do departamento)

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.DepartmentRepository;
 import com.devsuperior.aula.repositories.PersonRepository;
 
 @Service
@@ -14,13 +15,22 @@ public class PersonService {
 	@Autowired
 	private PersonRepository repository;
 	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	public PersonDepartmentDTO inserir(PersonDepartmentDTO dto) {
 		Person entity = new Person();
 		entity.setName(dto.getName());
 		entity.setSalary(dto.getSalary());
 		
-		Department dept = new Department();
-		dept.setId(dto.getDepartment().getId());
+		//nesse exemplo não retorna o nome do objeto, apenas cria um novo com as informações do json, no caso somente o id, sem nome, nao é um objeto monitorado pela jpa
+		//Department dept = new Department()
+		//dept.setId(dto.getDepartment().getId());
+		
+		
+		//nesse exemplo retorna o objeto completo do id informado, porque está sendo obtido o id do banxo
+		Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
+			
 		
 		entity.setDepartment(dept);
 		
